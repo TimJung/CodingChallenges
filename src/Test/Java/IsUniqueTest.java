28c0f9032e993dc26a6f4a728d5b4dffd4d10c7d
TimJung <tjung.dev@gmail.com>
2017-09-03 21:45:27 -0700

    Initial commit

---
+++ src/Test/Java/IsUniqueTest.java
@@ -0,0 +1,14 @@
+package Test.Java;
+
+import Main.Java.CrackingTheCodingInterview.ChapterOne.IsUnique;
+import org.junit.Test;
+
+/**
+ * Created by Tim on 9/3/2017.
+ */
+public class IsUniqueTest {
+    @Test
+    public void testQuestionOne(){
+
+    }
+}
